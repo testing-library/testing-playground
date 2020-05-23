241b6dadba3aa913a4b73f807018b26be2739ae2
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-24 00:43:42 +0200

    add base test for App.js
    to make App.js work I added a mock for createRange because it caused an exception with CodeMirror. I also add a mock for images

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,3 +1,14 @@
 import * as TestingLibraryDom from '@testing-library/dom';
 
 window.TestingLibraryDom = TestingLibraryDom;
+
+if (window.document) {
+  window.document.createRange = () => ({
+    setStart: () => {},
+    setEnd: () => {},
+    getBoundingClientRect: () => {
+      return { right: 0 };
+    },
+    getClientRects: () => [],
+  });
+}
