8f74ad4cb2aa1eccd1e90414953c8e17781181d4
Stephan Meijer <stephan.meijer@gmail.com>
2020-05-23 17:11:59 +0200

    feat: html preview area is now interactive

--- src/components/HtmlPreview.js
+++ src/components/HtmlPreview.js
@@ -1,12 +1,84 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import { useAppContext } from './Context.js';
+import prettyFormat from 'pretty-format';
+import { getQueryAdvise } from '../lib';
 
 function HtmlPreview({ html }, forwardRef) {
+  // Okay, listen up. `highlighted` can be a number of things, as I wanted to
+  // keep a single variable to represent the state. This to reduce bug count
+  // by creating out-of-sync states.
+  //
+  // 1. When the mouse pointer enters the preview area, `highlighted` changes
+  //    to true. True indicates that the highlight no longer indicates the parsed
+  //    element.
+  // 2. When the mouse pointer is pointing at an element, `highlighted` changes
+  //    to the target element. A dom node.
+  // 3. When the mouse pointer leaves that element again, `highlighted` changse
+  //    back to... true. Not to false! To indicate that we still want to use
+  //    the mouse position to control the highlight.
+  // 4. Once the mouse leaves the preview area, `highlighted` switches to false.
+  //    Indiating that the `parsed` element can be highlighted again.
+  const [highlighted, setHighlighted] = useState(false);
+  const { parsed, jsEditorRef } = useAppContext();
+
+  const { advise } = getQueryAdvise({
+    root: forwardRef.current,
+    element: highlighted,
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    jsEditorRef.current.setValue(advise.expression);
+  };
+
+  useEffect(() => {
+    if (highlighted) {
+      parsed.targets?.forEach((el) => el.classList.remove('highlight'));
+      highlighted.classList?.add('highlight');
+    } else {
+      highlighted?.classList?.remove('highlight');
+
+      if (highlighted === false) {
+        parsed.targets?.forEach((el) => el.classList.add('highlight'));
+      }
+    }
+
+    return () => highlighted?.classList?.remove('highlight');
+  }, [highlighted, parsed.targets]);
+
+  const handleMove = (event) => {
+    const target = document.elementFromPoint(event.clientX, event.clientY);
+    if (target === highlighted) {
+      return;
+    }
+
+    if (target === forwardRef.current) {
+      setHighlighted(true);
+      return;
+    }
+
+    setHighlighted(target);
+  };
+
   return (
     <div
-      className="preview"
-      ref={forwardRef}
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
+      className="relative flex flex-col"
+      onMouseEnter={() => setHighlighted(true)}
+      onMouseLeave={() => setHighlighted(false)}
+    >
+      <div
+        className="preview flex-auto"
+        ref={forwardRef}
+        dangerouslySetInnerHTML={{ __html: html }}
+        onClick={handleClick}
+        onMouseMove={handleMove}
+      />
+      <div className="p-2 bg-gray-200 rounded text-gray-800 font-mono text-xs">
+        {advise.expression
+          ? `> ${advise.expression}`
+          : '💡 try pointing and clicking at elements'}
+      </div>
+    </div>
   );
 }
 
---
+++ src/lib/queryAdvise.js
@@ -0,0 +1,57 @@
+import { messages, queries } from '../constants';
+import { getExpression } from './getExpression';
+import { computeAccessibleName, getRole } from 'dom-accessibility-api';
+
+export function getData({ root, element }) {
+  const type = element.getAttribute('type');
+  const tagName = element.tagName;
+
+  // prevent querySelector from tripping over corrupted html like <input id="button\n<button>
+  const id = (element.getAttribute('id') || '').split('\n')[0];
+  const labelElem = id ? root.querySelector(`[for="${id}"]`) : null;
+  const labelText = labelElem ? labelElem.innerText : null;
+
+  return {
+    role:
+      element.getAttribute('role') ||
+      // input's require a type for the role
+      (tagName === 'INPUT' && type !== 'text' ? '' : getRole(element)),
+    name: computeAccessibleName(element),
+    tagName: tagName,
+    type: type,
+    labelText: labelText,
+    placeholderText: element.getAttribute('placeholder'),
+    text: element.innerText,
+    displayValue: element.getAttribute('value'),
+
+    altText: element.getAttribute('alt'),
+    title: element.getAttribute('title'),
+
+    testId: element.getAttribute('data-testid'),
+  };
+}
+
+export function getQueryAdvise({ root, element }) {
+  if (!root || element?.nodeType !== Node.ELEMENT_NODE) {
+    return { data: {}, advise: {} };
+  }
+
+  const data = getData({ root, element });
+  const query = queries.find(({ method }) => getExpression({ method, data }));
+
+  if (!query) {
+    return {
+      level: 3,
+      expression: 'container.querySelector(…)',
+      ...messages[3],
+    };
+  }
+
+  const expression = getExpression({ method: query.method, data });
+  const advise = { expression, ...query, ...messages[query.level] };
+
+  return {
+    data,
+    advise,
+  };
+}
