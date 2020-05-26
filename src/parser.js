b6884a9bd949f6ea68f941b7f1cfe79d975a9647
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-26 09:21:43 +0200

    revert import

--- src/parser.js
+++ src/parser.js
@@ -7,8 +7,8 @@ import { queries as supportedQueries } from './constants';
 // It works when running parcel in dev mode, but not in build mode. Seems to
 // have something to do with a core-js Map polyfill being used?
 // It's now loaded from unpkg.com via ./index.html
-import { getQueriesForElement, queries, logDOM } from '@testing-library/dom';
-//const { getQueriesForElement, queries, logDOM } = window.TestingLibraryDom;
+//import { getQueriesForElement, queries, logDOM } from '@testing-library/dom';
+const { getQueriesForElement, queries, logDOM } = window.TestingLibraryDom;
 
 const debug = (element, maxLength, options) =>
   Array.isArray(element)
