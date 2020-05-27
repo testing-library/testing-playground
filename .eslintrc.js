dc10069d6078ca0c2165d3e78ad539cb1ce704b7
Stephan Meijer <stephan.meijer@gmail.com>
2020-05-27 19:52:48 +0200

    chore: add eslint-plugin-react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,25 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: [
+    'eslint:recommended',
     'plugin:prettier/recommended',
+    'plugin:react/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  rules: {
+    // I'll probably add some typescript types instead
+    'react/prop-types': 'off',
   },
 };
