ca72229f50f126eccd544d18b7038fc7a78a8d21
Jacob Evans <cloud887@gmail.com>
2020-05-29 23:07:31 -0500

    Added all Navigation to Tests -- Made check more specific URLs since shouldnt change often

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,6 +4,26 @@ describe('Navigation In Header Redirection Testing', () => {
     cy.get('.bg-gray-900 > .space-x-8 > a')
       .last()
       .should('have.attr', 'href')
-      .and('include', 'common-mistakes');
+      .and(
+        'include',
+        'https://kentcdodds.com/blog/common-mistakes-with-react-testing-library',
+      );
+
+    cy.get('.bg-gray-900 > .space-x-8 > a')
+      .eq(2)
+      .should('have.attr', 'href')
+      .and('include', 'https://testing-library.com/docs/guide-which-query');
+
+    cy.get('.bg-gray-900 > .space-x-8 > a')
+      .eq(1)
+      .should('have.attr', 'href')
+      .and(
+        'include',
+        'https://testing-library.com/docs/dom-testing-library/intro',
+      );
+    cy.get('.bg-gray-900 > .space-x-8 > a')
+      .eq(0)
+      .should('have.attr', 'href')
+      .and('include', 'https://github.com/smeijer/testing-playground');
   });
 });
