b23c5f2de4bc5e3b47cb812e6ab283685bab3e4c
Jacob Evans <cloud887@gmail.com>
2020-05-29 23:01:25 -0500

    Added Navigation Tests and fixed name convention

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,9 @@
 describe('Navigation In Header Redirection Testing', () => {
+  beforeEach(() => cy.visit('/'));
   it('Clicking "Common Mistakes" will redirect', () => {
-    // cy.get()
+    cy.get('.bg-gray-900 > .space-x-8 > a')
+      .last()
+      .should('have.attr', 'href')
+      .and('include', 'common-mistakes');
   });
 });
