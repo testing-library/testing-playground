ce10348e4c7e820202c1fab35f1ccd047a081791
marcosvega91 <marcomoretti0103@gmail.com>
2020-05-31 15:24:15 +0200

    add ensureArray test

---
+++ src/lib/ensureArray.test.js
@@ -0,0 +1,13 @@
+import { ensureArray } from './ensureArray';
+
+describe('ensureArray', () => {
+  it('should return an array if a non-array is passed', () => {
+    const resultArray = ensureArray('test');
+    expect(resultArray).toEqual(['test']);
+  });
+
+  it("should return an array if it's already of array type", () => {
+    const resultArray = ensureArray(['test']);
+    expect(resultArray).toEqual(['test']);
+  });
+});
