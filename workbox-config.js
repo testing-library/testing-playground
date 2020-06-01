4cd9ef2070159755c15a539418f07796c5391834
Dominik Guzy <dominik.guzy@gorrion.pl>
2020-06-01 18:00:23 +0200

    feat: workbox config part 2

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  globDirectory: 'dist',
+  globDirectory: 'dist/client',
   globPatterns: ['**/*.{html,js,css,png,svg,jpg,gif,json,ico,webmanifest}'],
   swDest: 'dist/client/sw.js',
   clientsClaim: true,
