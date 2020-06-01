ebfcbbefaf11e5aba35f8615e14f487cf9a0bf1f
Dominik Guzy <dominik.guzy@gorrion.pl>
2020-06-01 12:07:38 +0200

    feat: turn app into PWA

---
+++ src/worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'pwa-task-manager';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['pwa-task-manager'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
