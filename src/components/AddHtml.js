852bf0c5ee468fe61a7ac51e0488e7587633fc4f
Bianca Del Carretto <bianca.delcarretto@wyscout.com>
2020-06-04 22:30:09 +0200

    make image resize on reduced screens

--- src/components/AddHtml.js
+++ src/components/AddHtml.js
@@ -3,8 +3,13 @@ import icon from '../../public/code_thinking.png';
 
 function AddHtml() {
   return (
-    <div className="flex-auto h-1 flex flex-col relative overflow-scroll justify-center items-center">
-      <img className="opacity-50 h-48 w-full object-contain" src={icon} />
+    <div className="flex-auto absolute overflow-hidden h-full w-full flex flex-col justify-center items-center">
+      <div className="h-48 overflow-hidden w-full flex item-center">
+        <img
+          className="flex-auto resize opacity-50 object-contain max-h-full min-h-0"
+          src={icon}
+        />
+      </div>
       <p className="opacity-50 ml-24">Try adding some html</p>
     </div>
   );
