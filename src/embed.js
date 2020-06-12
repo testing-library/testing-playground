f4f8757d970b9a7936dda7af4e42f8dc883233d3
Stephan Meijer <stephan.meijer@gmail.com>
2020-06-12 10:38:16 +0200

    chore: remove useless comment block

--- src/embed.js
+++ src/embed.js
@@ -89,16 +89,3 @@ function onDocReady(fn) {
 }
 
 onDocReady(initPlaygrounds);
-
-//
-//
-//
-// <p className="codepen" data-height="265" data-theme-id="default" data-default-tab="js,result" data-user="smeijer"
-//    data-slug-hash="VPVqjZ"
-//    style="height: 265px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"
-//    data-pen-title="VPVqjZ">
-//   <span>See the Pen <a href="https://codepen.io/smeijer/pen/VPVqjZ">
-//   VPVqjZ</a> by Stephan Meijer (<a href="https://codepen.io/smeijer">@smeijer</a>)
-//   on <a href="https://codepen.io">CodePen</a>.</span>
-// </p>
-// <script async src="https://static.codepen.io/assets/embed/ei.js"></script>
