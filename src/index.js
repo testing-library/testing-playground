3fc11b5dfc28b3acf42f5715a7ff6485c5ef6e24
Stephan Meijer <stephan.meijer@gmail.com>
2020-06-12 14:45:42 +0200

    chore: remove unpkg import & move service-worker import (#140)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'regenerator-runtime/runtime';
 import 'react-toastify/dist/ReactToastify.min.css';
+import './service-worker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
