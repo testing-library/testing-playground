585e1611e6d56e0bb56c8193f6a7a685b19d36e2
ljosberinn <admin@gerritalex.de>
2020-06-12 22:40:09 +0200

    refactor: update oembed author name

--- src/lambda/oembed/oembed.js
+++ src/lambda/oembed/oembed.js
@@ -47,7 +47,7 @@ function handler(event, context, callback) {
         provider_name: 'Testing-Playground',
         provider_url: host,
 
-        author_name: 'Testing Playground',
+        author_name: 'Stephan Meijer',
         author_url: host,
 
         html: `<iframe src="${url}" width="${maxwidth}" height="${maxheight}" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" title="Testing Playground" style="overflow: hidden; display: block;" loading="lazy" name="testing-playground-${Date.now()}"></iframe>`,
