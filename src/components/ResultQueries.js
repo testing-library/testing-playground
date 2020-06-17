8f6ae76370d944902b56224a57090b9ab99f08e2
Marco Moretti <marcomoretti0103@gmail.com>
2020-06-17 11:48:26 +0200

    fix: use @testing-library/dom native methods for suggestion btns (#150)

--- src/components/ResultQueries.js
+++ src/components/ResultQueries.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { getExpression, getFieldName } from '../lib';
+import { getFieldName } from '../lib';
+import { queries } from '../constants';
 
 function Section({ children }) {
   return <div className="space-y-3">{children}</div>;
 }
 
 function Heading({ children }) {
-  return <h3 className="font-bold text-xs">{children}</h3>;
+  return <h3 className="text-xs font-bold">{children}</h3>;
 }
 
-const Field = React.memo(function Field({ method, data, dispatch, active }) {
+const Field = React.memo(function Field({
+  data,
+  method,
+  query,
+  dispatch,
+  active,
+}) {
   const field = getFieldName(method);
   const value = data[field];
-
   const handleClick = value
     ? () => {
-        const expression = getExpression({ method, data });
-        dispatch({ type: 'SET_QUERY', query: expression });
+        dispatch({
+          type: 'SET_QUERY',
+          query: `screen.${query.toString()}`,
+        });
       }
     : undefined;
 
@@ -26,7 +34,7 @@ const Field = React.memo(function Field({ method, data, dispatch, active }) {
       data-clickable={!!handleClick}
       onClick={handleClick}
     >
-      <div className="text-gray-800 font-light">{field}</div>
+      <div className="font-light text-gray-800">{field}</div>
       <div className="truncate">
         {value || <span className="text-gray-400">n/a</span>}
       </div>
@@ -35,68 +43,62 @@ const Field = React.memo(function Field({ method, data, dispatch, active }) {
 });
 
 // for inputs, the role will only work if there is also a type attribute
-function ResultQueries({ data, dispatch, activeMethod }) {
+function ResultQueries({ data, possibleQueries, dispatch, activeMethod }) {
   return (
     <div className="grid grid-cols-2 gap-4 pt-4">
       <Section>
         <Heading>1. Queries Accessible to Everyone</Heading>
-        <Field
-          method="getByRole"
-          data={data}
-          dispatch={dispatch}
-          active={'getByRole' === activeMethod}
-        />
-        <Field
-          method="getByLabelText"
-          data={data}
-          dispatch={dispatch}
-          active={'getByLabelText' === activeMethod}
-        />
-        <Field
-          method="getByPlaceholderText"
-          data={data}
-          dispatch={dispatch}
-          active={'getByPlaceholderText' === activeMethod}
-        />
-        <Field
-          method="getByText"
-          data={data}
-          dispatch={dispatch}
-          active={'getByText' === activeMethod}
-        />
-        <Field
-          method="getByDisplayValue"
-          data={data}
-          dispatch={dispatch}
-          active={'getByDisplayValue' === activeMethod}
-        />
+        {queries
+          .filter((query) => query.type === 'ACCESSIBLE')
+          .map((query) => {
+            return (
+              <Field
+                key={query.method}
+                data={data}
+                method={query.method}
+                query={possibleQueries[query.method]}
+                dispatch={dispatch}
+                active={query.method === activeMethod}
+              />
+            );
+          })}
       </Section>
 
       <div className="space-y-8">
         <Section>
           <Heading>2. Semantic Queries</Heading>
-          <Field
-            method="getByAltText"
-            data={data}
-            dispatch={dispatch}
-            active={'getByAltText' === activeMethod}
-          />
-          <Field
-            method="getByTitle"
-            data={data}
-            dispatch={dispatch}
-            active={'getByTitle' === activeMethod}
-          />
+          {queries
+            .filter((query) => query.type === 'SEMANTIC')
+            .map((query) => {
+              return (
+                <Field
+                  key={query.method}
+                  data={data}
+                  method={query.method}
+                  query={possibleQueries[query.method]}
+                  dispatch={dispatch}
+                  active={query.method === activeMethod}
+                />
+              );
+            })}
         </Section>
 
         <Section>
           <Heading>3. TestId</Heading>
-          <Field
-            method="getByTestId"
-            data={data}
-            dispatch={dispatch}
-            active={'getByTestId' === activeMethod}
-          />
+          {queries
+            .filter((query) => query.type === 'TEST')
+            .map((query) => {
+              return (
+                <Field
+                  key={query.method}
+                  data={data}
+                  method={query.method}
+                  query={possibleQueries[query.method]}
+                  dispatch={dispatch}
+                  active={query.method === activeMethod}
+                />
+              );
+            })}
         </Section>
       </div>
     </div>
--- src/lib/queryAdvise.test.js
+++ src/lib/queryAdvise.test.js
@@ -1,4 +1,4 @@
-import { getQueryAdvise } from './queryAdvise';
+import { getQueryAdvise, getAllPossibileQueries } from './queryAdvise';
 
 const emptyObject = `
   Object {
@@ -47,4 +47,112 @@ it('should add `screen.` on suggested query returned by getSuggestedQuery', () =
   const element = document.createElement('button');
   const result = getQueryAdvise({ rootNode, element });
   expect(result.suggestion.expression).toStartWith('screen.');
+});
+
+it('[getAllPossibileQueries] should return an object with all possibile queries', () => {
+  const rootNode = document.createElement('div');
+  const element = document.createElement('button');
+  rootNode.appendChild(element);
+  let suggestedQueries = getAllPossibileQueries(element);
+
+  expect(suggestedQueries).toMatchInlineSnapshot(`
+    Object {
+      "getByRole": Object {
+        "queryArgs": Array [
+          "button",
+        ],
+        "queryMethod": "getByRole",
+        "queryName": "Role",
+        "toString": [Function],
+        "variant": "get",
+      },
+    }
+  `);
+
+  rootNode.innerHTML = `
+    <label for="username">Username</label>
+    <input 
+      id="username"
+      name="username"
+      placeholder="how should I call you?" 
+      data-testid="uname"
+      title="enter your username"
+      alt="enter your username"
+      value="john-doe"
+      type="text"
+    />
+  `;
+  const input = rootNode.querySelector('input');
+  suggestedQueries = getAllPossibileQueries(input);
+
+  expect(suggestedQueries).toMatchInlineSnapshot(`
+    Object {
+      "getByAltText": Object {
+        "queryArgs": Array [
+          /enter your username/i,
+        ],
+        "queryMethod": "getByAltText",
+        "queryName": "AltText",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByDisplayValue": Object {
+        "queryArgs": Array [
+          /john-doe/i,
+        ],
+        "queryMethod": "getByDisplayValue",
+        "queryName": "DisplayValue",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByLabelText": Object {
+        "queryArgs": Array [
+          /username/i,
+        ],
+        "queryMethod": "getByLabelText",
+        "queryName": "LabelText",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByPlaceholderText": Object {
+        "queryArgs": Array [
+          /how should i call you\\?/i,
+        ],
+        "queryMethod": "getByPlaceholderText",
+        "queryName": "PlaceholderText",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByRole": Object {
+        "queryArgs": Array [
+          "textbox",
+          Object {
+            "name": /username/i,
+          },
+        ],
+        "queryMethod": "getByRole",
+        "queryName": "Role",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByTestId": Object {
+        "queryArgs": Array [
+          "uname",
+        ],
+        "queryMethod": "getByTestId",
+        "queryName": "TestId",
+        "toString": [Function],
+        "variant": "get",
+      },
+      "getByTitle": Object {
+        "queryArgs": Array [
+          /enter your username/i,
+        ],
+        "queryMethod": "getByTitle",
+        "queryName": "Title",
+        "toString": [Function],
+        "variant": "get",
+      },
+    }
+  `);
 });
