621e3cb9a5453a46f9070f0e2b15290f4f4afe28
Marco Moretti <marcomoretti0103@gmail.com>
2020-06-19 10:33:01 +0200

    feat: add copy to clipboard button for DomEvents (#194)

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+const variants = {
+  dark: 'text-gray-600 hover:text-gray-400',
+  light: 'text-gray-400 hover:text-gray-600',
+  white: 'text-white hover:text-white',
+};
+
 function IconButton({ children, title, variant, onClick, className }) {
+  const cssVariant = variants[variant] || variants['light'];
   return (
     <button
       className={[
         `pointer inline-flex focus:outline-none rounded-full flex items-center justify-center`,
-        variant === 'dark'
-          ? 'text-gray-600 hover:text-gray-400'
-          : 'text-gray-400 hover:text-gray-600',
+        cssVariant,
         className,
       ]
         .filter(Boolean)
